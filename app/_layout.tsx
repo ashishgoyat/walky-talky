29fd4f80ba2b69e70530bc089ca6f1762f501f06
Ashish Goyat <ashishgoyat.official@gmail.com>
2026-06-18 13:00:02 +0530

    setup nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView className="bg-white flex-1 p-4">
+      <View>
+        <Text className="text-rose-500">hlo there</Text>
+      </View>
+    </SafeAreaView>)
 }
